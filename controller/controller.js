60e067a0ccf6772e79cebb152df6530359c6d819
Hatshepsut <amabelle.delacruz322@gmail.com>
2019-03-24 22:59:41 -0700

    added comment button. having trouble with comment route

--- controller/controller.js
+++ controller/controller.js
@@ -1,177 +1,125 @@
 //DEPENDENCIES
 var express = require("express");
 var router = express.Router();
-var path = require("path");
-
-var request = require("request");
-var cheerio = require("cheerio");
+var axios = require("axios");
 
 //MODELS
 var Comment = require("../models/Comment.js");
 var Article = require("../models/Article.js");
 
-//ROUTES
-router.get("/", function(req, res) {
-  res.redirect("/articles");
+//LIBRARIES
+var cheerio = require("cheerio");
+var request = require("request");
+
+//HTML ROUTE
+router.get("/", function (req, res) {
+
+  Article.find({})
+    .populate("comments")
+    //EXECUTE QUERY
+    .exec(function (error, doc) {
+      //IF ERROR, LOG ERROR
+      if (error) {
+        console.log(error);
+      }
+      //OTHERWISE, SEND DOC
+      else {
+        console.log("all article with comments: " + doc);
+        res.render("index", { articles: doc });
+      }
+    });
 });
 
-//SCRAPE ROUTE
-router.get("/scrape", function(req, res) {
-  request("https://www.npr.org/sections/world/", function(error, response, html) {
+//API ROUTES
+router.get("/scrape", function (req, res) {
+  //REQUEST FROM NPR
+  axios.get("http://www.npr.org/sections/world/", function (error, response, html) {
     var $ = cheerio.load(html);
-    var titlesArray = [];
+    //CLASSES AND ELEMENTS PULLED FROM NPR
+    $(".item has-image").each(function (i, element) {
 
-    $(".title").each(function(i, element) {
-      var result = [];
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
-      //IF THERE IS A TITLE, SAVE TO DATABASE
-      if (result.title !== "" && result.link !== "") {
-        //UPDATES
-        if (titlesArray.indexOf(result.title) == -1) {
-          //SAVE TITLE TO ARRAY
-          titlesArray.push(result.title);
+      var result = {};
+      result.link = $(element).children(".item-info").children(".title").children().attr("href");
+      result.title = $(element).children(".item-info").children(".title").children().text();
 
-          //IF ARTICLE HAS NOT BEEN ADDED, ADD ARTICLE
-          Article.count({ title: result.title }, function(err, test) {
-            //IF TEST IS EQUAL TO 0, UNIQUE ARTICLE IS TRUE
-            if (test == 0) {
-              //CREATE NEW OBJECT FROM ARTICLE
-              var newEntry = new Article(result);
+      Article.findOne({ title: result.title }, function (err, data) {
 
-              //SAVE TO MONGODB
-              newEntry.save(function(err, doc) {
-                //IF ERROR LOG ERROR, OTHERWISE, CONSOLE LOG DOC.
-                if (err) {
-                  console.log(err);
-                } else {
-                  console.log(doc);
-                }
-              });
+        if (!data) {
+          var entry = new Article(result);
+
+          //SAVE TO DATABASE
+          entry.save(function (err, doc) {
+            //IF ERROR, LOG ERROR
+            if (err) {
+              console.log(err);
+            }
+            //OTHERWISE LOG DOC
+            else {
+              console.log("Saving " + doc.title);
             }
           });
+
         }
-        //IF ARTICLE EXISCTS, CONSOLE LOG THIS
         else {
-          console.log("Article exists");
+          console.log("Aricle " + data.title + " already in database");
         }
-        //OTHERWISE, CONSOLE LOG MISSING DATA
-      } else {
-        console.log("Missing data");
-      }
+      });
+
     });
     res.redirect("/");
   });
 });
 
-//GET ROUTE FOR ALL ARTICLES
-router.get("/articles", function(req, res) {
-  //SORTS NEWEST ARTICLES FIRST
-  Article.find()
-    .sort({ _id: -1 })
-    //VIEWS FOR HANDLEBARS
-    .exec(function(err, doc) {
-      if (err) {
-        console.log(err);
-      } else {
-        var artcl = { article: doc };
-        res.render("index", artcl);
+router.get("/article/:id", function (req, res) {
+  //FIND ARTICLE BY ID
+  Article.findOne({ "_id": req.params.id })
+    //GET ANY COMMENTS/NOTES
+    .populate("comments")
+    //EXECUTE
+    .exec(function (error, doc) {
+      //IF ERROR, LOG ERROR
+      if (error) {
+        console.log(error);
+      }
+      //OTHEWISE, RETURN DOC
+      else {
+        res.json(doc);
       }
     });
 });
-//GET ROUTE FOR SCRAPED ARTICLES IN DB
-router.get("/articles-json", function(req, res) {
-  Article.find({}, function(err, doc) {
-    if (err) {
-      console.log(err);
-    } else {
+
+router.get("/articles", function (req, res) {
+  //FIND ALL ARTICLES
+  Article.find({}, function (error, doc) {
+    //ERRORS
+    if (error) {
+      console.log(error);
+    }
+    else {
       res.json(doc);
     }
   });
 });
 
-//REMOVE ARTICLES
-router.get("/clearAll", function(req, res) {
-  Article.remove({}, function(err, doc) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("removed all articles");
-    }
-  });
-  res.redirect("/articles-json");
-});
-router.get("/readArticle/:id", function(req, res) {
-  var articleId = req.params.id;
-  var handlebarsOb = {
-    article: [],
-    body: []
-  };
+// //POST ROUTE FOR COMMENTS
+// router.post("/articles/:id", function(req, res) {
+//   // Create a new note and pass the req.body to the entry
+//   .Note.create(req.body)
+//     .then(function(dbNote) {
+//       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+//       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+//       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+//       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+//     })
+//     .then(function(dbArticle) {
+//       // If we were able to successfully update an Article, send it back to the client
+//       res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//       // If an error occurred, send it to the client
+//       res.json(err);
+//     });
+// });
 
-  // //FIND ONE ARTICLE BY ID
-  Article.findOne({ _id: articleId })
-    .populate("comment")
-    .exec(function(err, doc) {
-      if (err) {
-        console.log("Error: " + err);
-      } else {
-        handlebarsOb.article = doc;
-        var link = doc.link;
-        //REQUEST ARTICLE FROM LINK
-        request(link, function(error, res, html) {
-          var $ = cheerio.load(html);
 
-          $(".l-col__main").each(function(i, element) {
-            handlebarsOb.body = $(this)
-              .children(".c-entry-content")
-              .children("p")
-              .text();
-            //SEND ARTICLES TO VIEW
-            res.render("article", handlebarsOb);
-            
-            return false;
-          });
-        });
-      }
-    });
-});
-
-//COMMENT POST ROUTE
-router.post("/comment/:id", function(req, res) {
-  var user = req.body.name;
-  var content = req.body.comment;
-  var articleId = req.params.id;
-
-  var commentOb = {
-    name: user,
-    body: content
-  };
-  var newComment = new Comment(commentOb);
-  newComment.save(function(err, doc) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(doc._id);
-      console.log(articleId);
-      Article.findOneAndUpdate(
-        { _id: req.params.id },
-        { $push: { comment: doc._id } },
-        { new: true }
-      )
-        
-        .exec(function(err, doc) {
-          if (err) {
-            console.log(err);
-          } else {
-            res.redirect("/readArticle/" + articleId);
-            console.log(doc);
-          }
-        });
-    }
-  });
-});
 module.exports = router;
